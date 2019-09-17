8857fcc88e1603641f54508ab6d3649fa4e1c614
shevcari000 <shevcari000@CAM-LAB-27.bgsd.k12.wa.us>
2019-09-17 07:48:13 -0700

    updated

--- src/OldLady.java
+++ src/OldLady.java
@@ -3,7 +3,20 @@ public class OldLady {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Verse1() {
+		Verse2() {
+		Verse3() {
+		Verse4() {
+		Verse5() {
+		Verse6() {
+		
+	}
+	
+	public static void Verse1 () {
+		System.out.println ("There was an old lady who swallowed a fly.");
+		System.out.println ("I don't know why she swallowed a fly,");
+		System.out.println ("Perhaps she'll die");
+		System.out.println ("")
 	}
 
 }
