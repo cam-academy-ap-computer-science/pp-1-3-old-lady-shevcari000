d8939bca00085886eb313c4b33c11f02fa6caa98
shevcari000 <shevcari000@CAM-LAB-27.bgsd.k12.wa.us>
2019-09-19 07:02:18 -0700

    Finished

--- src/OldLady.java
+++ src/OldLady.java
@@ -3,20 +3,77 @@ public class OldLady {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Verse1() {
-		Verse2() {
-		Verse3() {
-		Verse4() {
-		Verse5() {
-		Verse6() {
+		Verse1();
+		Space();
+		Verse2();
+		Space();
+		Verse3();
+		Space();
+		Verse4();
+		Space();
+		Verse5();
+		Space();
+		Verse6();
 		
 	}
 	
 	public static void Verse1 () {
 		System.out.println ("There was an old lady who swallowed a fly.");
-		System.out.println ("I don't know why she swallowed a fly,");
+		System.out.println ("I don't know why she swallowed that fly,");
 		System.out.println ("Perhaps she'll die");
-		System.out.println ("")
 	}
+	
+	public static void Space () {
+		System.out.println ("");
+	}
+	
+	
+	public static void Verse2 () {
+		System.out.println ("There was an old lady who swallowed a spider,");
+		System.out.println ("That wriggled and iggled and jiggled inside her.");
+		System.out.println ("She swallowed the spider to catch the fly,");
+		System.out.println ("I don't know why she swallowed that fly,");
+		System.out.println ("Perhaps she'll die.");
+	}
+		
+	public static void Verse3 () {
+		System.out.println ("There was an old lady who swallowed a bird,");
+		System.out.println ("How absurd to swallow a bird.");
+		System.out.println ("She swallowed the bird to catch the spider,");
+		System.out.println ("She swallowed the spider to catch the fly,");
+		System.out.println ("I don't know why she swallowed the fly,");
+		System.out.println ("Perhaps she'll die");
+		
+	}
+	
+	public static void Verse4 () {
+		System.out.println ("There was an old lady who swallowed a cat,");
+		System.out.println ("Imagine that to swallow a cat.");
+		System.out.println ("She swallowed the cat to catch the bird,");
+		System.out.println ("She swallowed the bird to catch the spider,");
+		System.out.println ("She swallowed the spider to catch the fly,");
+		System.out.println ("I don't know why she swallowed that fly,");
+		System.out.println ("Perhaps she'll die.");
+		
+	}
+	
+	public static void Verse5 () {
+		System.out.println ("There was an old lady who swallowed a dog,");
+		System.out.println ("What a hog to swallow a dog.");
+		System.out.println ("She swallowed the dog to catch the cat,");
+		System.out.println ("She swallowed the cat to catch the bird,");
+		System.out.println ("She swallowed the bird to catch the spider,");
+		System.out.println ("She swallowed the spider to catch the fly,");
+		System.out.println ("I don't know why she swallowed that fly,");
+		System.out.println ("Perhaps she'll die.");
+		
+	}
+	
+	public static void Verse6 () {
+		System.out.println ("There was an old lady who swallowed a horse,");
+		System.out.println ("She died of course.");
+		
+	}
+		}
 
-}
+
